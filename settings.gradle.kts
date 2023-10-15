bac9e64a58f86d4b1fe32940c420bd62775cb869
sbdjay <piusd@shernet.sheridancollege.ca>
2023-10-14 23:33:27 -0400

    Creating Repo  commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FlowerShop - Android"
+include(":app")
+ 
